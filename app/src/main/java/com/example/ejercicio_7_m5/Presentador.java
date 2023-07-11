75dfe33a34ed7f5b5520be9adf15142caa02a34f
javo2023 <jguzmanalarcon@gmail.com>
2023-07-10 22:36:24 -0400

    VIsta presentador creado

--- app/src/main/java/com/example/ejercicio_7_m5/Presentador.java
+++ app/src/main/java/com/example/ejercicio_7_m5/Presentador.java
@@ -2,20 +2,30 @@ package com.example.ejercicio_7_m5;
 
 public class Presentador {
     private Modelo modelo;
+    private VistaPresentador vistaPresentador;
+
+    public Presentador(VistaPresentador vistaPresentador) {
+        this.vistaPresentador = vistaPresentador;
+
+        modelo =new Modelo();
+    }
 
     public void evaluarClave(String password){
         int nivelFuerza = modelo.validarClave(password);
 
         if(nivelFuerza==Modelo.DEBIL){
+           this.vistaPresentador.mostrarDebil();
 
         }
         else if(nivelFuerza==Modelo.MEDIA){
+            this.vistaPresentador.mostrarMedio();
 
         }
         else if(nivelFuerza==Modelo.FUERTE){
+            this.vistaPresentador.mostrarFuerte();
 
         }
-        else{
+        else{ this.vistaPresentador.mostrarError();
 
         }
 
---
+++ app/src/main/java/com/example/ejercicio_7_m5/VistaPresentador.java
@@ -0,0 +1,8 @@
+package com.example.ejercicio_7_m5;
+
+public interface VistaPresentador {
+    public void mostrarDebil();
+    public void mostrarMedio();
+    public void mostrarFuerte();
+    public void mostrarError();
+}
